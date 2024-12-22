48cc75050bf6d34c5f4e06873060c1a7b7dd1125
Евгений Кучма <desferto87@gmail.com>
2024-12-22 21:57:28 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace homeWorkPasswordProgramm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "123456";
+            int passwordTryCount = 2;
+            string userPasswordInput;
+
+            for (int i = 0; i <= passwordTryCount; i++)
+            {
+                Console.Write("Please enter password: ");
+                userPasswordInput = Console.ReadLine();
+
+                if (userPasswordInput == password)
+                {
+                    Console.WriteLine("secret massege");
+                    break;
+                }
+                else if (i == passwordTryCount)
+                {
+                    Console.WriteLine("attempts are over");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong password, please try again");
+                }
+            }
+        }
+    }
+}
